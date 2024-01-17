dc81bae1c54fa827a219a7e30aa2928d7c555509
dlstodfpwjsem <seojepar@student.42seoul.kr>
2024-01-17 20:50:56 +0900

    I don't fucking care about the unessentials

--- blank/linline.c
+++ blank/linline.c
@@ -6,7 +6,7 @@
 /*   By: seojeongpark <seojeongpark@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 12:50:42 by seojeongpar       #+#    #+#             */
-/*   Updated: 2024/01/17 17:21:59 by seojeongpar      ###   ########.fr       */
+/*   Updated: 2024/01/17 20:50:31 by seojeongpar      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void	plot_dot(int x, int y, void *mlx_ptr, void *win_ptr)
+void	plot_dot(t_dot d, void *mlx_ptr, void *win_ptr)
 {
-	mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0xFFFFFF);
+	mlx_pixel_put(mlx_ptr, win_ptr, d.cx, d.cy, d.color);
 }
 
 void	plot_line_high(t_dot d1, t_dot d2, void *mlx_ptr, void *win_ptr)
@@ -30,7 +30,6 @@ void	plot_line_high(t_dot d1, t_dot d2, void *mlx_ptr, void *win_ptr)
 	dx = d2.cx - d1.cx;
 	dy = d2.cy - d1.cy;
 	xi = 1;
-	// y1 < y2 인 상황이 보장되어 있는 경우
 	if (dx < 0)
 	{
 		xi = -1;
@@ -39,8 +38,8 @@ void	plot_line_high(t_dot d1, t_dot d2, void *mlx_ptr, void *win_ptr)
 	dis = 2 * dx - dy;
 	while (d1.cy <= d2.cy)
 	{
-		// printf("x is %d y is %d\n", d1.cx, d1.cy);
-		plot_dot(d1.cx, d1.cy++, mlx_ptr, win_ptr);
+		plot_dot(d1, mlx_ptr, win_ptr);
+		d1.cy++;
 		if (dis > 0)
 		{
 			d1.cx = d1.cx + xi;
@@ -61,7 +60,6 @@ void	plot_line_low(t_dot d1, t_dot d2, void *mlx_ptr, void *win_ptr)
 	dx = d2.cx - d1.cx;
 	dy = d2.cy - d1.cy;
 	yi = 1;
-	// x1 < x2 인 상황이 보장되어 있는 경우
 	if (dy < 0)
 	{
 		yi = -1;
@@ -70,8 +68,8 @@ void	plot_line_low(t_dot d1, t_dot d2, void *mlx_ptr, void *win_ptr)
 	dis = 2 * dy - dx;
 	while (d1.cx <= d2.cx)
 	{
-		// printf("x is %d y is %d\n", d1.cx, d1.cy);
-		plot_dot(d1.cx++, d1.cy, mlx_ptr, win_ptr);
+		plot_dot(d1, mlx_ptr, win_ptr);
+		d1.cx++;
 		if (dis > 0)
 		{
 			d1.cy = d1.cy + yi;
@@ -100,17 +98,24 @@ void	plot_line(t_dot d1, t_dot d2, void *mlx_ptr, void *win_ptr)
 	}
 }
 
-// int main()
-// {
-// 	void	*mlx_ptr = mlx_init();
-// 	void	*win_ptr = mlx_new_window(mlx_ptr, 1000, 1000, "Title");
-// 	t_dot	dotdot1;
-// 	t_dot	dotdot2;
+int	fp(int button, int x, int y, void *param)
+{
+	printf("button:%d, x:%d, y:%d\n", button, x, y);
+	return (1);
+}
 
-// 	dotdot1.cx = 1;
-// 	dotdot1.cy = 800;
-// 	dotdot2.cx = 500;
-// 	dotdot2.cy = 200;
-// 	plot_line(dotdot1, dotdot2, mlx_ptr, win_ptr);
-// 	mlx_loop(mlx_ptr);
-// }
+int main()
+{
+	void	*mlx_ptr = mlx_init();
+	void	*win_ptr = mlx_new_window(mlx_ptr, 1000, 1000, "Title");
+	t_dot	dotdot1;
+	t_dot	dotdot2;
+
+	dotdot1.cx = 1;
+	dotdot1.cy = 800;
+	dotdot2.cx = 500;
+	dotdot2.cy = 200;
+	plot_line(dotdot1, dotdot2, mlx_ptr, win_ptr);
+	mlx_loop(mlx_ptr);
+	mlx_mouse_hook(win_ptr, fp, (void *)0);
+}
