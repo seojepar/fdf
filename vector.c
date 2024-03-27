8830b38af8cb232021a04f883433dd131af28723
seojepar <146311443+seojepar@users.noreply.github.com>
2024-03-27 13:22:02 +0900

    -

--- vector.c
+++ vector.c
@@ -6,13 +6,14 @@
 /*   By: seojepar <seojepar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 21:15:25 by seojeongpar       #+#    #+#             */
-/*   Updated: 2024/03/27 13:20:06 by seojepar         ###   ########.fr       */
+/*   Updated: 2024/03/27 13:21:34 by seojepar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-t_vect	make_vect(double cx, double cy, t_ptr *ptr){
+t_vect	make_vect(double cx, double cy, t_ptr *ptr)
+{
 	double	ox;
 	double	oy;
 	t_vect	ret;
@@ -24,10 +25,12 @@ t_vect	make_vect(double cx, double cy, t_ptr *ptr){
 	return (ret);
 }
 
-double	vect_size(t_vect A){
+double	vect_size(t_vect A)
+{
 	return (sqrt(A.x * A.x + A.y * A.y));
 }
 
-int	vect_ip(t_vect A, t_vect B){
+int	vect_ip(t_vect A, t_vect B)
+{
 	return (A.x * B.x + A.y * B.y);
 }
